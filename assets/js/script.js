9acdcbd5d8a1fba0307ce48abefd8fd58e75eb98
LewisC0 <lewiscarroll1347@Gmail.com>
2022-08-18 20:21:35 +0000

    add javascript code to create two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,84 @@
-console.log('hello world');
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert('You clicked submit!');
+            } else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+/**
+ * The main game loop, called then the script is first loaded
+ * and after the users answer has been processed.
+ */
+function runGame(){
+
+    // creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num2 = Math.floor(Math.random()*25)+1;
+
+}
+
+
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+
+
+
+
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+
+
+
+
+
+
+
+function displayAdditionQuestion(){
+    
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivisionQuestion(){
+
+}
