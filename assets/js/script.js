352ff47eaade1eca41f168f9a26c3e399f1b049f
LewisC0 <lewiscarroll1347@Gmail.com>
2022-08-18 20:27:02 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert('You clicked submit!');
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType)
             }
         })
     }
+
+    runGame("addition");
 
 })
 
@@ -28,11 +30,18 @@ document.addEventListener("DOMContentLoaded", function(){
  * The main game loop, called then the script is first loaded
  * and after the users answer has been processed.
  */
-function runGame(){
+function runGame(gameType){
 
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -67,8 +76,10 @@ function incrementWrongAnswer(){
 
 
 
-function displayAdditionQuestion(){
-    
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion(){
